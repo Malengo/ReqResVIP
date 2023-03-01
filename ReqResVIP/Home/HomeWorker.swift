50ef6cbe9541fb16c36ddeb1e64449eb605b9546
José Augusto <jmalengo@brq.com>
2023-03-01 15:56:01 -0300

    error handling in api resquest

--- ReqResVIP/Home/HomeWorker.swift
+++ ReqResVIP/Home/HomeWorker.swift
@@ -15,22 +15,37 @@ class HomeWorker: HomeWorkerProtocol {
     
     func getAll() {
         Task {
-            await request()
+            do {
+                try await request()
+            } catch let error {
+                print(error)
+            }
         }
     }
     
-    private func request() async {
-        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+    private func request() async throws {
+        guard let url = URL(string: "https://reqres.in/api/users") else {
+            throw ResponseError.invalidUrl
+        }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw ResponseError.invalidData
+            }
+            
+            guard httpResponse.statusCode == 200 else {
+                throw ResponseError.httpStatusCodeError(httpResponse.statusCode)
+            }
+            
             let employer = try JSONDecoder().decode(Users.self, from: data)
-            print(employer.data.first)
+            
         } catch let error {
-            print(error)
+            throw error is DecodingError ? ResponseError.decodeError : ResponseError.invalidUrl
         }
     }
 }
